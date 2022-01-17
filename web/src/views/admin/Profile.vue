09d70da6e87e83f3221ac858e202521f05f729c9
renhj <1761179051@qq.com>
2022-01-17 13:51:06 +0800

    add user profile

---
+++ web/src/views/admin/Profile.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="profile">
+        profile
+    </div>
+</template>
+
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="scss" scoped>
+.profile {
+    width: 100%;
+    height: 100%;
+}
+</style>
