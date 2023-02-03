d792b0a709f8dd7e72cb155143f5c73832cc5b08
renhj <1761179051@qq.com>
2023-02-03 09:22:39 +0800

    settings.vue

--- web/src/views/admin/Settings.vue
+++ web/src/views/admin/Settings.vue
@@ -1,6 +1,31 @@
 <template>
-    <div class="settings">
-        settings
+    <div class="container">
+        <div class="container-header">
+      <header class="header-content">
+        <h2 class="header-title">
+          <router-link to="/admin/settings">Posts</router-link>
+        </h2>
+      </header>
+    </div>
     </div>
 </template>
 
+<script>
+export default {
+    data(){
+        return {
+            
+        }
+    },
+    mounted(){
+
+    },
+    methods: {
+        
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
